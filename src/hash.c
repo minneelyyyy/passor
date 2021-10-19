5afff9c5bdf5468992dffd07f2caa6c773e666d4
April Kahny <aprilkahny@gmail.com>
2021-10-19 13:41:45 -0400

    update to use tabs instead of spaces
    Signed-off-by: April Kahny <aprilkahny@gmail.com>

--- src/hash.c
+++ src/hash.c
@@ -1,11 +1,11 @@
 
 unsigned long hash(unsigned char *str)
 {
-    unsigned long hash = 5381;
-    int c;
+	unsigned long hash = 5381;
+	int c;
 
-    while (c = *str++)
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	while (c = *str++)
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
-    return hash;
+	return hash;
 }
