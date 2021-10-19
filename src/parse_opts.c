756cf2566333ea8fc83428ff48b5b423680ff041
April Kahny <aprilkahny@gmail.com>
2021-10-19 15:57:30 -0400

    begone magic numbers
    Signed-off-by: April Kahny <aprilkahny@gmail.com>

--- src/parse_opts.c
+++ src/parse_opts.c
@@ -4,25 +4,33 @@
 #include "mode.h"
 #include "hash.h"
 
+
+/* very funny magic numbers */
+#define NO_NUMBERS  12144439425868588933U
+#define NO_UPPER    249805550247202768U
+#define NO_LOWER    8243583158168875026U
+#define NO_SYMBOLS  12144439432482481042U
+
+
 void parse_opts(struct mode *m, int argc, char *argv[])
 {
 	for (int i = 1; i < argc; i++)
 	{
 		switch (hash(argv[i]))
 		{
-			case 1462069125:
+			case NO_NUMBERS:
 				m->numbers = false;
 				break;
 
-			case 224907216:
+			case NO_UPPER:
 				m->upper = false;
 				break;
 
-			case 3138154514:
+			case NO_LOWER:
 				m->lower = false;
 				break;
 
-			case 3780993938:
+			case NO_SYMBOLS:
 				m->symbols = false;
 				break;
 		
