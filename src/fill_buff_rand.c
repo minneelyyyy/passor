fa5a08e4653abb31ac8c71da2e050e82d17f755e
April Kahny <aprilkahny@gmail.com>
2021-10-19 14:25:52 -0400

    add fill_buff_rand
    Signed-off-by: April Kahny <aprilkahny@gmail.com>

---
+++ src/fill_buff_rand.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "mode.h"
+
+void fill_buff_rand(char *str, struct mode m, FILE *random)
+{
+
+}
---
+++ src/fill_buff_rand.h
@@ -0,0 +1,9 @@
+#ifndef FILL_BUFF_RAND_H
+#define FILL_BUFF_RAND_H
+
+#include <stdio.h>
+#include "mode.h"
+
+void fill_buff_rand(char *str, struct mode m, FILE *random);
+
+#endif /* FILL_BUFF_RAND_H */
