2ffcac23a8a5c6d4590b4bd4098dbeff8956f36b
April Kahny <aprilkahny@gmail.com>
2021-10-20 09:31:26 -0400

    move text to nearest 8th column
    Signed-off-by: April Kahny <aprilkahny@gmail.com>

--- src/main.c
+++ src/main.c
@@ -17,10 +17,10 @@ void help(int exit_status)
 		"Usage: %s [OPTION]... [LENGTH]\n"
 		"generate a password / random string\n\n"
 
-		"  --no-upper          generate without uppercase characters\n"
-		"  --no-lower          generate without lowercase characters\n"
-		"  --no-numbers        generate without number characters\n"
-		"  --no-symbols        generate without other characters\n\n",
+		"  --no-upper            generate without uppercase characters\n"
+		"  --no-lower            generate without lowercase characters\n"
+		"  --no-numbers          generate without number characters\n"
+		"  --no-symbols          generate without other characters\n\n",
 		program_name
 	);
 
