0ac18a9f910ec561fbe41d9a10669165be4ed264
April Kahny <aprilkahny@gmail.com>
2021-10-20 09:56:36 -0400

    add more options, add argument verification
    Signed-off-by: April Kahny <aprilkahny@gmail.com>

--- src/parse_opts.c
+++ src/parse_opts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
 #include "mode.h"
 #include "hash.h"
@@ -10,6 +11,23 @@
 #define NO_UPPER    8243583158179576629U
 #define NO_LOWER    8243583158168875026U
 #define NO_SYMBOLS  12144439432482481042U
+#define NUMBER		7569865168420680U
+#define ALPHA       229389837851557U
+#define ALPHA_NUM   13783808862824347298U
+
+
+static bool is_zero(char *str)
+{
+	const int len = strlen(str);
+
+	for (int i = 0; i < len; i++)
+	{
+		if (str[i] != '0')
+			return false;
+	}
+
+	return true;
+}
 
 
 void parse_opts(struct mode *m, int argc, char *argv[])
@@ -30,12 +48,35 @@ void parse_opts(struct mode *m, int argc, char *argv[])
 				m->lower = false;
 				break;
 
-			case NO_SYMBOLS:
+			case NO_SYMBOLS: case ALPHA_NUM:
 				m->symbols = false;
+				break;
+			
+			case NUMBER:
+				m->symbols = false;
+				m->upper = false;
+				m->lower = false;
+				break;
+			
+			case ALPHA:
+				m->symbols = false;
+				m->numbers = false;
 				break;
 		
 			default:
-				m->length = atoi(argv[i]);
+			{
+				int arg = atoi(argv[i]);
+
+				if (arg == 0 && !is_zero(argv[i]))
+				{
+					fprintf(stderr, "error: %s: not a valid argument\n", argv[i]);
+					exit(1);
+				}
+				else
+				{
+					m->length = arg;
+				}
+			}
 		}
 	}
 }
