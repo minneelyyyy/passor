b0eb0adc08bc7dfbf48504ebf240ed6e19b3b5e8
April Kahny <aprilkahny@gmail.com>
2021-10-27 11:26:45 -0400

    refactor struct mode
    Signed-off-by: April Kahny <aprilkahny@gmail.com>

--- src/mode.h
+++ src/mode.h
@@ -5,10 +5,28 @@
 
 struct mode
 {
-	bool lower   : 1;
-	bool upper   : 1;
-	bool numbers : 1;
-	bool symbols : 1;
+	// flags for basic generation
+	union {
+		unsigned basic : 4;
+		struct {
+			bool lower   : 1;
+			bool upper   : 1;
+			bool numbers : 1;
+			bool symbols : 1;
+			bool spaces  : 1;
+		};
+	};
+
+	// special generation modes
+	union {
+		unsigned special : 2;
+		struct {
+			bool base64 : 1;
+			bool base16 : 1;
+		};
+	};
+
+	// other
 	char characters_not_allowed[64];
 	int length;
 };
