cd31e4c947a80d62b54d0eb4e3e6e57aaa3b5286
April Kahny <aprilkahny@gmail.com>
2021-10-27 11:49:46 -0400

    fix whitespace
    Signed-off-by: April Kahny <aprilkahny@gmail.com>

--- src/parse_opts.c
+++ src/parse_opts.c
@@ -47,10 +47,10 @@
 		}                                                              \
 	}
 
-#define common_flag_check(b, default, str) \
-	{\
-		check_special(str);\
-		check_bool_set_twice(b, default, str);\
+#define common_flag_check(b, default, str)             \
+	{                                              \
+		check_special(str);                    \
+		check_bool_set_twice(b, default, str); \
 	}
 
 /* check if a string is a zero, as in "0" or "000" */
