07663ae1e71ca7c7f47c62065ac7208f39d3b8d2
April Kahny <aprilkahny@gmail.com>
2021-10-27 11:26:12 -0400

    add special generation modes
    Signed-off-by: April Kahny <aprilkahny@gmail.com>

--- src/fill_buff_rand.c
+++ src/fill_buff_rand.c
@@ -2,35 +2,56 @@
 #include <string.h>
 #include "mode.h"
 
+#define BASE64 0b01
+#define BASE16 0b10
+
 #define inrange(c, a, z) a <= c && c <= z
 
 static char get_rand_char(struct mode m, FILE *random)
 {
-	char c;
+	static const char *base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789/+";
+	static const char *base16_chars = "0123456789ABCDEF";
 
-	for (;;)
+	if (m.special)
 	{
-		c = getc(random) % ('~' - ' ' + 1) + ' ';
+		switch (m.special)
+		{
+			case BASE64:
+				return base64_chars[getc(random) % strlen(base64_chars)];
 
-		if (!m.upper && inrange(c, 'A', 'Z'))
-			continue;
+			case BASE16:
+				return base16_chars[getc(random) % strlen(base16_chars)];
+		}
+	}
+	else
+	{
+		for (;;)
+		{
+			char c = getc(random) % ('~' - ' ' + 1) + ' ';
 
-		if (!m.lower && inrange(c, 'a', 'z'))
-			continue;
+			if (!m.upper && inrange(c, 'A', 'Z'))
+				continue;
 
-		if (!m.numbers && inrange(c, '0', '9'))
-			continue;
+			if (!m.lower && inrange(c, 'a', 'z'))
+				continue;
 
-		if (!m.symbols && !!strchr("`~!@#$%^&*()-=_+[]{}\\|;:'\",<.>/?", c))
-			continue;
+			if (!m.numbers && inrange(c, '0', '9'))
+				continue;
 
-		if (!!strchr(m.characters_not_allowed, c))
-			continue;
+			if (!m.symbols && !!strchr("`~!@#$%^&*()-=_+[]{}\\|;:'\",<.>/?", c))
+				continue;
 
-		break;
+			if (!m.spaces && c == ' ')
+				continue;
+
+			if (!!strchr(m.characters_not_allowed, c))
+				continue;
+
+			return c;
+		}
 	}
 
-	return c;
+	return 0;
 }
 
 void fill_buff_rand(char *str, struct mode m, FILE *random)
