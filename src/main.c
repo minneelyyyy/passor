bd8f925ee348b55204a3ceb0efb9fec7a46f9670
April Kahny <aprilkahny@gmail.com>
2021-10-27 11:25:08 -0400

    update debug message and defaults
    Signed-off-by: April Kahny <aprilkahny@gmail.com>

--- src/main.c
+++ src/main.c
@@ -55,6 +55,9 @@ int main(int argc, char *argv[])
 		.upper   = true,
 		.numbers = true,
 		.symbols = true,
+		.spaces  = false,
+		.base64  = false,
+		.base16  = false,
 		.characters_not_allowed = " ",
 		.length  = 8
 	};
@@ -69,9 +72,11 @@ int main(int argc, char *argv[])
 			".lower                  = %d\n"
 			".numbers                = %d\n"
 			".symbols                = %d\n"
+			".base64                 = %d\n"
+			".base16                 = %d\n"
 			".characters_not_allowed = \"%s\"\n"
 			".length                 = %d\n",
-			options.lower, options.upper, options.numbers, options.symbols, options.characters_not_allowed, options.length
+			options.lower, options.upper, options.numbers, options.symbols, options.base64, options.base16, options.characters_not_allowed, options.length
 		);
 	#endif
 
